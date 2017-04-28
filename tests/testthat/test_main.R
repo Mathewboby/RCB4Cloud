c4beb665f189fce3db38605de2ba7fe4d47f5adf
Shane Ryerson <shane.ryerson@monsanto.com>
2017-04-28 11:30:30 -0500

    Made test and readme slightly better

--- tests/testthat/test_main.R
+++ tests/testthat/test_main.R
@@ -21,6 +21,5 @@ test_that("Checking the ouput against RCBInputExample for path P4", {
                         all.y = FALSE )
   comp_table[, c("diff", "p.diff", "sed")] <- round(comp_table[, c("diff", "p.diff", "sed")], 4)
   
-  # Expect the differences in rounding to be less than the precision of the rounding
-  expect_identical(abs(comp_table$MEAN_DELTA - comp_table$diff ) < 0.00011, rep(TRUE, nrow(comp_table) ) )
+  expect_equal(comp_table$MEAN_DELTA, comp_table$diff, tolerance=1.0e-4 )
 })
