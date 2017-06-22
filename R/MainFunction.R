f8fc522bb0fd5707fb876c8b3e35966b4bf344dc
Li, Xiao <xiao.li@monsanto.com>
2017-06-22 17:28:35 -0500

    Update MainFunction.R

--- R/MainFunction.R
+++ R/MainFunction.R
@@ -5,14 +5,14 @@
 #' @param analysis_type A string specifying the type of analysis to be conducted.
 #' @param alpha A number between 0 and 1 specifying the confidence level
 #' @param data_fields A list specifying the environmental variables to be used in fitting the model and the associated column names in the data dataframe.
-#' @return A list with a component for the fit model, the LS means analysis, and the delta means analysis
+#' @return A list with several components for the fit model, if analysis_type is "P1", "P2" or "P4", returned list includes delta table, LS table, ANOVA table for fixed effects, variance component table and residual table; if analysis_type is "P3" or "P5", returned list includes LS table, variance component table, residual table and BLUP table for factor1 
 #'
 #' @importFrom asreml asreml
 #' @export
 
 R.ASReml_RCB_Return <- function(data,
                                 analysis_type,
-                                alpha = .05,
+                                alpha = .1,
                                 data_fields =
                                   list(
                                     CROP_OBSRVTN_DETAIL_ID = "numValue",
@@ -46,8 +46,9 @@ R.ASReml_RCB_Return <- function(data,
   start <- Sys.time()
   message(paste("Running ASReml using analysis type:", analysis_type), appendLF = TRUE )
 
+ 
   # Run R-ASReml, capture output
-  RCB_asr  <-  asreml(fixed = fixed_formula,
+  RCB_asr  <-  asreml::asreml(fixed = fixed_formula,
                       random = random_formula,
                       tolerance = 1E-10,
                       data = data,
@@ -69,46 +70,64 @@ R.ASReml_RCB_Return <- function(data,
                                      estimable = FALSE,
                                      xform = list()),
                       workspace = 1000 * 1e6 / 8,
-                      pworkspace = 1000 * 1e6 / 8)
+                      pworkspace = 1000 * 1e6 / 8, trace=F)
 
+  
   message("finished in ", round(difftime(Sys.time(), start, units = time.scale[1]), digits = 2), " ", time.scale)
-  message("Creating output files...", appendLF = FALSE)
+  message("Creating output files...", appendLF = TRUE)
 
   # Out_return$console$modeling <- RCB_asr
-
+  
+  if (analysis_type %in% c('P3','P5') ) {
   # LS Means
   Out_return$LSM_TABLE <- lsmAnalysis_r(RCB_asr, data)
   
   ## ANOVA table
   Out_return$var_analysis <- ANOVA_output_r(RCB_asr)
   
+  ## add residual tables
+  Out_return$resid <- resid_table(RCB_asr,data)
+  colnames(Out_return$resid) <- c('residuals',FIELD_ID,REP_ID)
+  
+  ## add blup table for random effects
+  Out_return$BLUP <- as.matrix(blup_table(RCB_asr,analysis_type))
+  colnames(Out_return$BLUP) <- c('BLUP for random effects')
+  
+  }
   
   if (analysis_type %in% c('P1','P2','P4') ) {
-  Out_return$LSM_TABLE <- lsmAnalysis(RCB_asr, data, alpha=alpha)    
   
+  # to use wald.asreml() only once to save computational time  
+  LSM_ALL <- lsmAnalysis(RCB_asr, data, alpha=alpha)    
+  
+  # basic LSM table
+  Out_return$LSM_TABLE <- LSM_ALL[[1]]
   # Degrees of freedom
-  degrees_freedom = asreml::wald.asreml( RCB_asr,denDF ="default",data=data)[[1]][,2]  ## a vector including all the fixed effect
+  degrees_freedom = LSM_ALL[[2]]  ## a vector including all the fixed effect
 
   
   del=deltaAnalysis(RCB_asr,alpha=alpha, degrees_freedom[2])
   # Deltas and p-values
   Out_return$Deltas <- del[[1]]
   # Mean Separation Grouping
-  mean_sep_group <- del[[2]]$M
+  mean_sep_group <- del[[2]]
   
   ## combine LSM_TABLE and MSG
   Out_return$LSM_TABLE <- cbind(Out_return$LSM_TABLE,mean_sep_group)
+  
   ## sort by descending order
   Out_return$LSM_TABLE <- Out_return$LSM_TABLE[order(Out_return$LSM_TABLE$Yield,decreasing = TRUE),]
   
+  
   ## ANOVA table
-  Out_return$var_analysis <- ANOVA_output(RCB_asr,degrees_freedom)
-  }
+  Out_return$aov <- LSM_ALL[[3]]
+  Out_return$varcomp <- LSM_ALL[[4]]
   
   ## add residual tables
   Out_return$resid <- resid_table(RCB_asr,data)
   colnames(Out_return$resid) <- c('residuals',FIELD_ID,REP_ID)
   
+ }
 
   return(Out_return)
 }
